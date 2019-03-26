fcb318dbdaa3aca8f8cee2d2088983094e863a95
Brian Warner <warner@lothar.com>
2019-03-26 15:08:32 -0700

    add sourcemap to the generated source. doesn't work yet.

--- src/build-source-bundle.js
+++ src/build-source-bundle.js
@@ -8,6 +8,7 @@ export default async function bundleSource(startFilename) {
   });
   const { output } = await bundle.generate({
     format: 'cjs',
+    sourcemap: 'inline',
   });
   if (output.length !== 1) {
     throw Error('unprepared for more than one chunk/asset');
@@ -16,7 +17,11 @@ export default async function bundleSource(startFilename) {
     throw Error(`unprepared for assets: ${output[0].fileName}`);
   }
   let { code: source } = output[0];
-  // const { map: sourceMap } = output[0];
+  const { map: sourceMap } = output[0];
+  const sourcemapURL = `data:application/json;charset=utf-8;base64,${Buffer.from(
+    JSON.stringify(sourceMap),
+  ).base64Slice()}`;
+  source += `//# sourceMappingURL=${sourcemapURL}\n`;
 
   // 'source' is now a string that contains a program, which references
   // require() and sets module.exports . This is close, but we need a single
