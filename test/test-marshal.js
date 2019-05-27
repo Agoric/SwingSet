52a463aee95d801c0410c050197bdeae65020dde
Mark S. Miller <erights@gmail.com>
2019-05-26 22:43:21 -0700

    lint-ok

--- test/test-marshal.js
+++ test/test-marshal.js
@@ -141,8 +141,10 @@ test('serialize ibid cycle', t => {
 test('forbid ibid cycle', t => {
   const m = makeMarshal();
   const uns = val => m.unserialize(val, []);
-  t.throws(() => uns('["a",{"@qclass":"ibid","index":0},"c"]'),
-           /Ibid cycle at 0/);
+  t.throws(
+    () => uns('["a",{"@qclass":"ibid","index":0},"c"]'),
+    /Ibid cycle at 0/,
+  );
   t.end();
 });
 
