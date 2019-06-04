deefd84cff312416957e387f31319470b5074ee0
Dan Connolly <dckc@madmode.com>
2019-06-03 23:41:45 -0500

    insist takes truthy (mixed), not just boolean

--- collections/insist.js
+++ collections/insist.js
@@ -15,7 +15,7 @@ import harden from '@agoric/harden';
 // console only. We assume only the virtual platform's owner can read
 // what is written to the console, where the owner is in a privileged
 // position over computation running on that platform.
-function insist(flag /* : boolean */) {
+function insist(flag /* : mixed */) {
   function tag(template /* : string[] */, ...args /* : mixed[] */) {
     if (flag) {
       return;
