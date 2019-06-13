5a2df06360ac8fe7acb2c11c6ecb79a1498c8727
Kate Sills <kate@agoric.com>
2019-06-13 08:26:21 +0200

    add test for whether calling addEgress with the same information twice throws

--- test/commsSlots/commsController/test-addEgress.js
+++ test/commsSlots/commsController/test-addEgress.js
@@ -58,3 +58,77 @@ test('handleCommsController addEgress', t => {
   t.deepEqual(actualMeToYouSlot, meToYouSlot);
   t.end();
 });
+
+test('handleCommsController addEgress - same twice', t => {
+  let fulfillToDataArgs;
+
+  const mockSyscall = {
+    fulfillToData(...args) {
+      fulfillToDataArgs = args;
+    },
+  };
+
+  const state = makeState();
+
+  const resolverID = 2;
+  const sender = 'user';
+  const index = 4;
+  const valslot = { '@qclass': 'slot', index: 0 };
+  const caps = [{ type: 'import', id: 10 }];
+  const helpers = {
+    log: console.log,
+  };
+
+  const result = handleCommsController(
+    state,
+    mockSyscall,
+    'addEgress',
+    JSON.stringify({
+      args: [sender, index, valslot],
+    }),
+    caps,
+    resolverID,
+    helpers,
+  );
+
+  t.equal(result, undefined);
+
+  const UNDEFINED = JSON.stringify({ '@qclass': 'undefined' });
+
+  // ensure calls to syscall are correct
+  t.deepEqual(fulfillToDataArgs, [resolverID, UNDEFINED, []]);
+
+  // ensure state updated correctly
+  const youToMeSlot = {
+    type: 'your-egress',
+    id: index,
+  };
+  const meToYouSlot = state.clists.changePerspective(youToMeSlot);
+  const kernelToMeSlot = state.clists.mapIncomingWireMessageToKernelSlot(
+    sender,
+    youToMeSlot,
+  );
+  const {
+    meToYouSlot: actualMeToYouSlot,
+  } = state.clists.mapKernelSlotToOutgoingWireMessage(caps[0], sender);
+  t.deepEqual(kernelToMeSlot, caps[0]); // actual, expected
+  t.deepEqual(actualMeToYouSlot, meToYouSlot);
+
+  // do it again, should receive an error
+  t.throws(
+    () =>
+      handleCommsController(
+        state,
+        mockSyscall,
+        'addEgress',
+        JSON.stringify({
+          args: [sender, index, valslot],
+        }),
+        caps,
+        resolverID,
+        helpers,
+      ),
+    'kernel-import-10 already exists in clist',
+  );
+  t.end();
+});
