3dc6669d1e09ac4f69ac8013f87eab2e7f0c1a64
Michael FIG <mfig@agoric.com>
2019-06-19 14:35:51 -0600

    ustore: move to userland so harden works

--- src/devices/ustore-src.js
+++ src/devices/ustore-src.js
@@ -1,27 +1,194 @@
 import harden from '@agoric/harden';
 
 export default function setup(syscall, state, helpers, endowments) {
-  const { bridge, connect } = endowments;
+  const { bridge, stores } = endowments;
+  const buffered = {};
 
   return helpers.makeDeviceSlots(
     syscall,
     state,
     s => {
-      connect(s);
-      return harden({
-        write(addr, key, str) {
-          return bridge.write(addr, key, str);
+      const { SO, getDeviceState, setDeviceState } = s;
+      let watchers = getDeviceState() || {};
+
+      let handlers;
+
+      function cloneWithException(obj, key, fn) {
+        const newObj = {};
+        Object.keys(obj).forEach(k => {
+          if (k === key) {
+            const newVal = fn();
+            if (newVal !== undefined) {
+              newObj[k] = newVal;
+            }
+          } else {
+            newObj[k] = obj[k];
+          }
+        });
+        return newObj;
+      }
+
+      function notifyAll(store, addr, key) {
+        // console.log(`notifying ${addr} ${key}`);
+        addr = String(addr);
+        key = String(key);
+        handlers.notify(addr, key, handlers.read(addr, key));
+        const storekey = key === '' ? 'ustore' : `ustore.${key}`;
+        const ret = store.sendMsg(
+          JSON.stringify({ method: 'keys', key: storekey }),
+        );
+        // console.log(`got keys ${addr} ${storekey}`, ret);
+        const keys = JSON.parse(ret);
+        for (const child of keys) {
+          const subKey = key === '' ? child : `${key}.${child}`;
+          notifyAll(store, addr, subKey);
+        }
+      }
+
+      bridge.afterSetStore = function afterSetStore(addr, store) {
+        // Work through the watchers and send notifications that changed.
+        notifyAll(store, addr, '');
+
+        // Perform any buffered requests.
+        const bufs = buffered[addr];
+        delete buffered[addr];
+        if (bufs) {
+          for (const [key, data] of bufs) {
+            // console.log(`Buffered ${addr} ${key}`, data);
+            handlers.write(addr, key, data);
+          }
+        }
+      };
+
+      handlers = harden({
+        notify(addr, key, data) {
+          // console.log(`actually notifying ${addr} ${key} ${data}`);
+          const wlist = watchers[addr];
+          if (!wlist) {
+            // console.log(`No watchers for ${addr}`);
+            return;
+          }
+          const ws = wlist[key];
+          if (!ws) {
+            // console.log(`No key watchers for ${addr} ${key}`);
+            return;
+          }
+          const [objs, lastData] = ws;
+          data = String(data);
+          if (data === lastData) {
+            // Don't notify, no change.
+            // console.log(`No change for ${data}`);
+            return;
+          }
+
+          // Update the lastData.
+          const newWlist = { ...wlist, [key]: [objs, data] };
+          watchers = { ...watchers, [addr]: newWlist };
+          // console.log('notify watchers', inspect(watchers));
+          setDeviceState(harden(watchers));
+
+          for (const obj of objs) {
+            console.log(`calling ${obj} with ${data}`);
+            SO(obj).nextUserState(key, data);
+          }
         },
+
+        write(addr, key, data) {
+          const store = stores[addr];
+          if (!store) {
+            console.log(
+              `stores[${addr}] doesn't exist, buffering ${key}`,
+              data,
+            );
+            if (!buffered[addr]) {
+              buffered[addr] = [];
+            }
+            buffered[addr].push([key, data]);
+            return;
+          }
+          store.sendMsg(
+            JSON.stringify({
+              method: 'set',
+              key: `ustore.${key}`,
+              value: data,
+            }),
+          );
+          // console.log(`Written ${addr} ${key}`, data, ret);
+          handlers.notify(addr, key, data);
+        },
+
         read(addr, key) {
-          return bridge.read(addr, key);
+          addr = String(addr);
+          const store = stores[addr];
+          if (!store) {
+            throw Error(`${addr} reader not yet registered`);
+          }
+          key = String(key);
+          const storekey = key === '' ? `ustore` : `ustore.${key}`;
+          return JSON.parse(
+            store.sendMsg(JSON.stringify({ method: 'get', key: storekey })),
+          );
         },
-        notify(addr, key, str) {
-          return bridge.notify(addr, key, str);
+
+        watch(addr, key, obj) {
+          let wlist = watchers[addr];
+          if (!wlist) {
+            wlist = {};
+          }
+          key = String(key);
+          let ws = wlist[key];
+          if (!ws) {
+            ws = [[], null];
+          }
+          const [objs, lastData] = ws;
+          if (objs.indexOf(obj) >= 0) {
+            throw Error(`Object ${obj} is already listening`);
+          }
+          // watchers is immutable, so reconstruct it.
+          ws = [[...objs, obj], lastData];
+          wlist = { ...wlist, [key]: ws };
+          watchers = { ...watchers, [addr]: wlist };
+          // console.log('watch watchers', inspect(watchers));
+          setDeviceState(harden(watchers));
+          if (lastData !== null) {
+            // Bring the listener up-to-speed.
+            SO(obj).next(lastData);
+          }
+          return harden({
+            unwatch() {
+              return handlers.unwatch(addr, key, obj);
+            },
+          });
         },
-        watch(addr, key, fn) {
-          return bridge.watch(addr, key, fn);
+
+        unwatch(addr, key, obj) {
+          const wlist = watchers[addr];
+          if (!wlist) {
+            return;
+          }
+          const ws = wlist[key];
+          if (!ws) {
+            return;
+          }
+          const [objs, lastData] = ws;
+
+          const index = objs.indexOf(obj);
+          if (index < 0) {
+            throw Error(`${obj} is already unwatched`);
+          }
+
+          const newObjs = objs.filter(o => o !== obj);
+          const newWlist = cloneWithException(wlist, key, () =>
+            newObjs.length > 0 ? [newObjs, lastData] : undefined,
+          );
+          watchers = cloneWithException(watchers, addr, () =>
+            Object.keys(newWlist).length > 0 ? newWlist : undefined,
+          );
+          // console.log('unwatch watchers', inspect(watchers));
+          setDeviceState(harden(watchers));
         },
       });
+      return handlers;
     },
     helpers.name,
   );
--- src/devices/ustore.js
+++ src/devices/ustore.js
@@ -17,186 +17,27 @@
   is watching changes.
 */
 
-import harden from '@agoric/harden';
-/*
-import util from 'util';
-const inspect = (obj) => util.inspect(obj, false, 10);
-*/
-
 export default function buildUserStore() {
   const stores = {};
-  let watchers = {}; // persisted in device state
+  const bridge = {};
   const srcPath = require.resolve('./ustore-src');
-
-  let mySO;
-  let mySetDeviceState;
-
-  function cloneWithException(obj, key, fn) {
-    const newObj = {};
-    Object.keys(obj).forEach(k => {
-      if (k === key) {
-        const newVal = fn();
-        if (newVal !== undefined) {
-          newObj[k] = newVal;
-        }
-      } else {
-        newObj[k] = obj[k];
-      }
-    });
-    return newObj;
-  }
-
-  // not hardened
-  const bridge = {
-    notify(addr, key, data) {
-      const wlist = watchers[addr];
-      if (!wlist) {
-        return;
-      }
-      const ws = wlist[key];
-      if (!ws) {
-        return;
-      }
-      const [objs, lastData] = ws;
-      const thisData = data === undefined ? lastData : data;
-      if (thisData === null) {
-        // Don't notify, no data.
-        return;
-      }
-      if (`${thisData}` === lastData) {
-        // Don't notify, no change.
-      }
-      if (!mySO) {
-        throw Error(`notify before connect`);
-      }
-
-      // Update the thisData.
-      const newWlist = { ...wlist, [key]: [objs, thisData] };
-      watchers = { ...watchers, [addr]: newWlist };
-      // console.log('notify watchers', inspect(watchers));
-      mySetDeviceState(harden(watchers));
-
-      for (const obj of objs) {
-        mySO(obj).next(thisData);
-      }
-    },
-
-    write(addr, key, data) {
-      const store = stores[addr];
-      if (!store) {
-        throw Error(`${addr} writer not yet registered`);
-      }
-      store.sendMsg(JSON.stringify({ method: 'set', key, value: data }));
-      bridge.notify(addr, key, data);
-    },
-
-    read(addr, key) {
-      addr = String(addr);
-      const store = stores[addr];
-      if (!store) {
-        throw Error(`${addr} reader not yet registered`);
-      }
-      key = String(key);
-      return store.sendMsg(JSON.stringify({ method: 'get', key }));
-    },
-
-    watch(addr, key, obj) {
-      let wlist = watchers[addr];
-      if (!wlist) {
-        wlist = {};
-      }
-      key = String(key);
-      let ws = wlist[key];
-      if (!ws) {
-        ws = [[], null];
-      }
-      const [objs, lastData] = ws;
-      if (objs.indexOf(obj) >= 0) {
-        throw Error(`Object ${obj} is already listening`);
-      }
-      // watchers is immutable, so reconstruct it.
-      ws = [[...objs, obj], lastData];
-      wlist = { ...wlist, [key]: ws };
-      watchers = { ...watchers, [addr]: wlist };
-      // console.log('watch watchers', inspect(watchers));
-      mySetDeviceState(harden(watchers));
-      if (lastData !== null) {
-        // Bring the listener up-to-speed.
-        mySO(obj).next(lastData);
-      }
-      return harden({
-        unwatch() {
-          return bridge.unwatch(addr, key, obj);
-        },
-      });
-    },
-
-    unwatch(addr, key, obj) {
-      const wlist = watchers[addr];
-      if (!wlist) {
-        return;
-      }
-      const ws = wlist[key];
-      if (!ws) {
-        return;
-      }
-      const [objs, lastData] = ws;
-
-      const index = objs.indexOf(obj);
-      if (index < 0) {
-        throw Error(`${obj} is already unwatched`);
-      }
-
-      const newObjs = objs.filter(o => o !== obj);
-      const newWlist = cloneWithException(wlist, key, () =>
-        newObjs.length > 0 ? [newObjs, lastData] : undefined,
-      );
-      watchers = cloneWithException(watchers, addr, () =>
-        Object.keys(newWlist).length > 0 ? newWlist : undefined,
-      );
-      // console.log('unwatch watchers', inspect(watchers));
-      mySetDeviceState(harden(watchers));
-    },
-  };
-
-  function notifyAll(addr, key) {
-    addr = String(addr);
-    const store = stores[addr];
-    if (!store) {
-      throw Error(`No store for address ${addr}`);
-    }
-    key = String(key);
-    bridge.notify(addr, key);
-    const ret = store.sendMsg(JSON.stringify({ method: 'keys', key }));
-    const keys = JSON.parse(ret);
-    for (const child of keys) {
-      const subKey = key === '' ? child : `${key}.${child}`;
-      notifyAll(addr, subKey);
-    }
-  }
 
   function setStore(addr, store) {
     if (store) {
       stores[addr] = store;
 
-      // Now work through the watchers and send notifications that changed.
-      notifyAll(addr, '');
+      // console.log(`FIGME: Calling`, bridge.afterSetStore);
+      if (bridge.afterSetStore) {
+        bridge.afterSetStore(addr, store);
+      }
     } else {
       delete stores[addr];
     }
   }
 
-  function connect({ SO, getDeviceState, setDeviceState }) {
-    mySO = SO;
-    watchers = getDeviceState() || {};
-    mySetDeviceState = setDeviceState;
-  }
-
   return {
     srcPath,
-    endowments: { bridge, connect },
+    endowments: { bridge, stores },
     setStore, // for external access
-    ...bridge, // these are for external access
-    bridge, // for debugging/testing
   };
 }
