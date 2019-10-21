ec989af1a4e4d1eb361e3e17324528a9fb532faf
Chris Hibbert <hibbert@agoric.com>
2019-10-21 15:25:24 -0700

    This version supports persistence in scenario 3.
    Persist by saving state as arrays, and copying them before calling getState().
    Handle revival.
    both repeaters and handlers can be removed.
    Rescheduling of handlers on repetition works.

--- src/vats/vat-timerWrapper.js
+++ src/vats/vat-timerWrapper.js
@@ -4,16 +4,21 @@ import { insist } from '../insist';
 
 function build(E, D) {
   let timerNode; // Timer device
+  const repeaters = new Map();
 
-  function registerTimerDevice(TimerDevnode) {
-    timerNode = TimerDevnode;
+  function registerTimerDevice(TimerDeviceNode) {
+    timerNode = TimerDeviceNode;
   }
 
   async function createTimerHandler() {
     return harden({
+      getCurrentTimestamp() {
+        return nat(D(timerNode).getLastPolled());
+      },
       setWakeup(delaySecs, handler) {
         return D(timerNode).setWakeup(delaySecs, handler);
       },
+      // can be used after setWakeup(h) or schedule(h)
       removeWakeup(handler) {
         return D(timerNode).removeWakeup(handler);
       },
@@ -23,16 +28,18 @@ function build(E, D) {
           `createRepeater takes two numbers as arguments. ${delaySecs}, ${interval}`,
         );
 
-        const r = D(timerNode).createRepeater(delaySecs, interval);
+        const index = D(timerNode).createRepeater(delaySecs, interval);
 
         const vatRepeater = harden({
-          schedule(handler) {
-            return D(r).schedule(handler);
+          schedule(h) {
+            return D(timerNode).schedule(index, h);
           },
           disable() {
-            return D(r).disable();
+            repeaters.delete(index);
+            return D(timerNode).deleteRepeater(index);
           },
         });
+        repeaters.set(index, vatRepeater);
         return vatRepeater;
       },
     });
